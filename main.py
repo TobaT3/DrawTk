e69cb7f0a76b4abec7b26d8ad283d0a6c4c17378
TobaT3 <tomassolcan2009@gmail.com>
2022-10-03 18:23:42 +0200

    graphical overhaul, delete object functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from glob import escape
 import tkinter
 from tkinter import HORIZONTAL, colorchooser, filedialog, messagebox, font
+import customtkinter
 from PIL import ImageGrab
 import pathlib
 import tkfontchooser
@@ -8,7 +9,9 @@ import tkfontchooser
 #import pyi_splash #doesnt actually import anything; but should be uncommented when building with pyinstaller so the splash screen closes
 #pyi_splash.close()
 
-win = tkinter.Tk()
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("blue")
+win = customtkinter.CTk()
 win.resizable(False, False)
 win.title("TkPaint Canvas")
 #win.iconbitmap("drawtk.ico")
@@ -42,6 +45,7 @@ getfirstpoint = True
 drawingngon = False
 text = ""
 font = "Helvetica 15"
+fontstr = "Helvetica 15"
 textid = 999999999999
 
 clickcs = []
@@ -64,12 +68,11 @@ def callback(e):
     if roundto == 0:
         roundto=1
 
-    poslabel.config(text="snapped to "+str(round(x/roundto)*roundto)+"x, "+str(round(y/roundto)*roundto)+"y")
+    poslabel.configure(text="snapped to "+str(round(x/roundto)*roundto)+"x, "+str(round(y/roundto)*roundto)+"y")
     #print("Pointer is currently at %d, %d" %(x,y))
 
 def click(e):
     global xc1,xc2,yc1,yc2,xc3,yc3,clickc,roundto, exportlines, ids, getfirstpoint, clickcs, drawingngon, linethicc, linec, totids, thicc, textid, font
-    
     
     ids = totids
     clickc += 1
@@ -159,6 +162,9 @@ def click(e):
         totids += 1
         textid = totids
 
+    if mode == 'delete': # thanks https://stackoverflow.com/questions/38982313/python-tkinter-identify-object-on-click
+        c.delete(c.find_closest(e.x, e.y))
+
 def onKeyPress(event):
     global textid, totids, text, totids, ids, xc1,xc2, font
     
@@ -205,10 +211,12 @@ def drawngon():
     
     print("end drawngon")    
 def donetext():
-    global textid, totids, text, totids, ids, xc1,xc2, font
+    global textid, totids, text, totids, ids, xc1,xc2, font, exportlines
 
     c.delete(textid)
     c.create_text(xc1,yc1,text=text, font=font)
+    exportlines.append("c.create_text("+str(xc1)+','+str(yc1)+", text='"+text+"', font='"+str(font)+"')")
+    print(exportlines)
     ids += 1
     totids += 1
     textid = 999999
@@ -220,7 +228,22 @@ win.bind('<Motion>',callback)
 win.bind("<Button-1>", click)
 win.bind('<KeyPress>', onKeyPress)
 
+def modedelete():
+    global mode, rectanglebutton,ovalbutton,linebutton,trianglebutton,ngonbutton,clickc
+    clickc = 0
+    mode = "delete"
+    print(mode)
 
+    rectanglebutton.configure(bg="#BDBDBD")
+    ovalbutton.configure(bg="#FFFFFF")
+    linebutton.configure(bg="#FFFFFF")
+    trianglebutton.configure(bg="#FFFFFF")
+    ngonbutton.configure(bg="#FFFFFF")
+    textbutton.configure(bg="#FFFFFF")
+
+    if mode=="text":
+        donetext()
+        textwindow.destroy()
 def moderectangle():
     global mode, rectanglebutton,ovalbutton,linebutton,trianglebutton,ngonbutton,clickc
     clickc = 0
@@ -234,7 +257,9 @@ def moderectangle():
     ngonbutton.configure(bg="#FFFFFF")
     textbutton.configure(bg="#FFFFFF")
 
-    textwindow.destroy()
+    if mode=="text":
+        donetext()
+        textwindow.destroy()
 def modeoval():
     global mode, rectanglebutton,ovalbutton,linebutton,trianglebutton,ngonbutton,clickc
     clickc = 0
@@ -247,7 +272,9 @@ def modeoval():
     ngonbutton.configure(bg="#FFFFFF")
     textbutton.configure(bg="#FFFFFF")
 
-    textwindow.destroy()
+    if mode=="text":
+        donetext()
+        textwindow.destroy()
 def modeline():
     global mode, rectanglebutton,ovalbutton,linebutton,trianglebutton,ngonbutton,clickc
     clickc = 0
@@ -260,7 +287,9 @@ def modeline():
     ngonbutton.configure(bg="#FFFFFF")
     textbutton.configure(bg="#FFFFFF")
 
-    textwindow.destroy()
+    if mode=="text":
+        donetext()
+        textwindow.destroy()
 def modetriangle():
     global mode, rectanglebutton,ovalbutton,linebutton,trianglebutton,ngonbutton,clickc
     clickc = 0
@@ -273,7 +302,9 @@ def modetriangle():
     ngonbutton.configure(bg="#FFFFFF")
     textbutton.configure(bg="#FFFFFF")
 
-    textwindow.destroy()
+    if mode=="text":
+        donetext()
+        textwindow.destroy()
 def modengon():
     global mode, rectanglebutton,ovalbutton,linebutton,trianglebutton,ngonbutton,clickc,getfirstpoint, drawingngon
     clickc = 0
@@ -296,7 +327,9 @@ def modengon():
     ngonbutton.configure(bg="#BDBDBD")
     textbutton.configure(bg="#FFFFFF")
 
-    textwindow.destroy()
+    if mode=="text":
+        donetext()
+        textwindow.destroy()
 def modetext():
     global mode, rectanglebutton,ovalbutton,linebutton,trianglebutton,ngonbutton,clickc,getfirstpoint, drawingngon, toolwindow, textwindow, textwindow
     clickc = 0
@@ -326,7 +359,7 @@ def fontchooser():
     fontbutton.configure(text="Change font and size ("+fontstr+")")
 
 def createtextwindow():
-    global textwindow, fontbutton
+    global textwindow, fontbutton, font, fontstr
 
     textwindow = tkinter.Toplevel(toolwindow)
     textwindow.geometry("300x100")
@@ -338,7 +371,7 @@ def createtextwindow():
     y = toolwindow.winfo_y()
     textwindow.geometry(f'{300}x{60}+{x-300}+{y+235}')
 
-    fontbutton = tkinter.Button(textwindow, text="Change font and size (Calibri 16)", command=fontchooser)
+    fontbutton = tkinter.Button(textwindow, text="Change font and size "+str(fontstr), command=fontchooser)
     fontbutton.pack()
 
     donebutton = tkinter.Button(textwindow, text="DONE", command=donetext)
@@ -346,8 +379,6 @@ def createtextwindow():
 
     textwindow.bind('<KeyPress>', onKeyPress) # so you can type in both windows; annoyed me when you couldnt
     
-
-
 
 def opencolorpick():
     global fillcolor,colpickbut
@@ -369,12 +400,12 @@ def togglefilltransparent():
         isfilltransparent = False
 
         filltransparentbut.configure(bg="#FFFFFF")
-        filltransparentbut.config(text="Toggle Fill Transparent (False)")
+        filltransparentbut.configure(text="Toggle Fill Transparent (False)")
     else:
         isfilltransparent = True
 
         filltransparentbut.configure(bg="#BDBDBD")
-        filltransparentbut.config(text="Toggle Fill Transparent (True)")
+        filltransparentbut.configure(text="Toggle Fill Transparent (True)")
 
 def load():
     global ids, totids
@@ -443,85 +474,106 @@ def deleteall():
         c.delete("all")
         exportlines.clear()
 
-toolwindow = tkinter.Toplevel(win)
+toolwindow = customtkinter.CTkToplevel(win)
 toolwindow.geometry("300x700")
 toolwindow.title("Toolbox")
-toolwindow.resizable(False, False)
+#toolwindow.resizable(False, False)
 
 screen_width = toolwindow.winfo_screenwidth()
 screen_height = toolwindow.winfo_screenheight()
 center_x = int(screen_width/2 - 700 / 2)
 center_y = int(screen_height/2 - 500 / 2)
-toolwindow.geometry(f'{300}x{700}+{center_x-310}+{center_y}')
+toolwindow.geometry(f'{300}x{700}+{center_x-310}+{center_y-175}')
 
 def movetoolwin(e):
     x = win.winfo_x()
     y = win.winfo_y()
 
-    toolwindow.geometry(f'{300}x{700}+{x-310}+{y}')
+    toolwindow.geometry(f'{300}x{875}+{x-310}+{y-175}')
 
     if mode == "text":
         x = toolwindow.winfo_x()
         y = toolwindow.winfo_y()
         textwindow.geometry(f'{300}x{60}+{x-300}+{y+235}')
 
-
 win.bind('<Configure>', movetoolwin)
 
 #lbl = tkinter.Label(toolwindow, text="I am in this toolwindow thing right").pack()
 
 
-poslabel = tkinter.Label(toolwindow, text="", font="Roboto 14")
-poslabel.pack(pady=10)
+poslabel = customtkinter.CTkLabel(toolwindow, text="e", text_font="Roboto 14")
+poslabel.pack(pady=5)
 
-undobutton = tkinter.Button(toolwindow, text="Undo", command=undo, bg="light green").pack()
+undobutton = customtkinter.CTkButton(toolwindow, text="Undo", command=undo, fg_color="green", hover_color="dark green").pack(pady=5)
+delobjbutton = customtkinter.CTkButton(toolwindow, text="Delete object", command=modedelete, bg="#BDBDBD")
+delobjbutton.pack(pady=5)
 
-toollabel = tkinter.Label(toolwindow, text="Tools", font="Roboto 14").pack()
-rectanglebutton = tkinter.Button(toolwindow, text="Rectangle", command=moderectangle, bg="#BDBDBD")
-rectanglebutton.pack()
-ovalbutton = tkinter.Button(toolwindow, text="Oval", command=modeoval)
-ovalbutton.pack()
-linebutton = tkinter.Button(toolwindow, text="Line (uses OUTLINE color not FILL COLOR", command=modeline)
-linebutton.pack()
-trianglebutton = tkinter.Button(toolwindow, text="Triangle", command=modetriangle)
-trianglebutton.pack()
-ngonbutton = tkinter.Button(toolwindow, text="Polygon (click to start drawing)", command=modengon)
-ngonbutton.pack()
-textbutton = tkinter.Button(toolwindow, text="Text", command=modetext)
-textbutton.pack()
+toollabel = customtkinter.CTkLabel(toolwindow, text="Tools", text_font="Roboto 14").pack(pady=5)
+rectanglebutton = customtkinter.CTkButton(toolwindow, text="Rectangle", command=moderectangle, bg="#BDBDBD")
+rectanglebutton.pack(pady=5)
+ovalbutton = customtkinter.CTkButton(toolwindow, text="Oval", command=modeoval)
+ovalbutton.pack(pady=5)
+linebutton = customtkinter.CTkButton(toolwindow, text="Line (uses OUTLINE color not FILL COLOR", command=modeline)
+linebutton.pack(pady=5)
+trianglebutton = customtkinter.CTkButton(toolwindow, text="Triangle", command=modetriangle)
+trianglebutton.pack(pady=5)
+ngonbutton = customtkinter.CTkButton(toolwindow, text="Polygon (click to start drawing)", command=modengon)
+ngonbutton.pack(pady=5)
+textbutton = customtkinter.CTkButton(toolwindow, text="Text", command=modetext)
+textbutton.pack(pady=5)
 
-collabel = tkinter.Label(toolwindow, text="Colors and thickness", font="Roboto 14").pack()
-colpickbut = tkinter.Button(toolwindow, text="Fill Color", command=opencolorpick, bg=fillcolor)
-colpickbut.pack()
+collabel = customtkinter.CTkLabel(toolwindow, text="Colors and thickness", text_font="Roboto 14").pack(pady=10)
+colpickbut = customtkinter.CTkButton(toolwindow, text="Fill Color", command=opencolorpick, bg=fillcolor)
+colpickbut.pack(pady=5)
 
-filltransparentbut = tkinter.Button(toolwindow, text="Toggle Fill Transparent (True)", command=togglefilltransparent, bg=("#BDBDBD"))
-filltransparentbut.pack()
+filltransparentbut = customtkinter.CTkButton(toolwindow, text="Toggle Fill Transparent (True)", command=togglefilltransparent, bg=("#BDBDBD"))
+filltransparentbut.pack(pady=5)
 
-colpickbut2 = tkinter.Button(toolwindow, text="Line Color", command=opencolorpick2, bg=linecolor)
-colpickbut2.pack()
+colpickbut2 = customtkinter.CTkButton(toolwindow, text="Outline Color", command=opencolorpick2, bg=linecolor)
+colpickbut2.pack(pady=5)
 
-thicc = tkinter.Scale(toolwindow, from_=1, to=75, orient=HORIZONTAL, tickinterval=10, length=200, label="Line Width", resolution=5)
+def thiccupgrade(value):
+    global thiccvaluelabel
+    if value == 0:
+        thiccvaluelabel.configure(text="Off")
+    else:    
+        thiccvaluelabel.configure(text=str(round((value))))
+
+snappinglabel = customtkinter.CTkLabel(toolwindow, text="Snapping").pack(pady=5)
+thicc = customtkinter.CTkSlider(toolwindow, from_=1, to=75, orient=HORIZONTAL, width=200, number_of_steps=5) # label="Line Width",
 thicc.pack()
+thicc.set(linethicc)
+thiccvaluelabel = customtkinter.CTkLabel(toolwindow, text="50")
+thiccvaluelabel.pack(pady=0)
 
-deletebut = tkinter.Button(toolwindow, text="Load", command=load).pack()
+deletebut = customtkinter.CTkButton(toolwindow, text="Load", command=load).pack(pady=5)
 
-setlabel = tkinter.Label(toolwindow, text="Other", font="Roboto 14").pack()
+setlabel = customtkinter.CTkLabel(toolwindow, text="Other", text_font="Roboto 14").pack(pady=5)
 
-snapp = tkinter.Scale(toolwindow, from_=0, to=300, orient=HORIZONTAL, length=200, label="Snapping to every (0 to turn off)", resolution=5)
+def snappupgrade(value):
+    global snappingvaluelabel
+    if value == 0:
+        snappingvaluelabel.configure(text="Off")
+    else:    
+        snappingvaluelabel.configure(text=str(round((value))))
+
+snappinglabel = customtkinter.CTkLabel(toolwindow, text="Snapping").pack(pady=5)
+snapp = customtkinter.CTkSlider(toolwindow, from_=0, to=300, orient=HORIZONTAL, width=200, number_of_steps=60, command=snappupgrade) #label="Snapping to every (0 to turn off)",
 snapp.pack()
 snapp.set(roundto)
+snappingvaluelabel = customtkinter.CTkLabel(toolwindow, text="50")
+snappingvaluelabel.pack(pady=0)
 
-
-explabel = tkinter.Label(toolwindow, text="Save and Export", font="Roboto 14").pack()
+explabel = customtkinter.CTkLabel(toolwindow, text="Save and Export", text_font="Roboto 14").pack(pady=5)
 #warnlabel2 = tkinter.Label(toolwindow, text="WARNING: Will overwrite previously generated files", fg="red").pack()
 #warnlabel3 = tkinter.Label(toolwindow, text="Copy the tkinter_drawing files somewhere else \n if you dont want to lose them", fg="red").pack()
-exportbut = tkinter.Button(toolwindow, text="Save - export to .py", command=exportpy).pack()
-imgbut = tkinter.Button(toolwindow, text="export to image", command=exportpng).pack()
+exportbut = customtkinter.CTkButton(toolwindow, text="Save - export to .py", command=exportpy).pack(pady=5)
+imgbut = customtkinter.CTkButton(toolwindow, text="export to image", command=exportpng).pack(pady=5)
 
-warnlabel = tkinter.Label(toolwindow, text="WARNING: Will delete everything", fg="red").pack()
-deletebut = tkinter.Button(toolwindow, text="CLEAR CANVAS", command=deleteall, bg="red").pack()
+#warnlabel = customtkinter.CTkLabel(toolwindow, text="WARNING: Will delete everything", fg="red").pack(pady=5)
+deletebut = customtkinter.CTkButton(toolwindow, text="CLEAR CANVAS", command=deleteall, fg_color="red", hover_color="red").pack(pady=5)
 
-melabel = tkinter.Label(toolwindow, text="Made by TobaT3", font="Roboto 8").pack()
+melabel = customtkinter.CTkLabel(toolwindow, text="Made by TobaT3", text_font="Roboto 8").pack()
 
 
 win.mainloop()
